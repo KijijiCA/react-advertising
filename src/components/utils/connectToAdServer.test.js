0881ff8d0357676a2ac2df096a7364f18988cb79
Patrick Hund <pahund@team.mobile.de>
2018-04-24 09:22:39 +0200

    added unit test for connectToAdServer
    Change-Id: Idfecb26e66ff968372759a26a083c757fe6554a5

---
+++ src/components/utils/connectToAdServer.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import connectToAdServer from './connectToAdServer';
+import expectSnapshot from '@mt-testutils/expect-snapshot';
+
+jest.mock('../../AdvertisingContext', () => ({
+    Consumer({ children }) {
+        return children('activate');
+    }
+}));
+
+describe('When I connect a component to the ad server', () => {
+    let ConnectedComponent;
+    beforeEach(() => (ConnectedComponent = connectToAdServer(props => <div {...props} />)));
+    describe('the connected component', () =>
+        it('renders correctly (activate prop is added, other props are proxied through)', () =>
+            expectSnapshot(<ConnectedComponent foo="bar" />)));
+});
