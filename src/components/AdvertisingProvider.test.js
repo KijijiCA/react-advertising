2c18b39ec403c6dc63470a76e29f8d4f954ff4b2
Patrick Hund <pahund@team.mobile.de>
2018-04-27 10:23:36 +0300

    unit test for advertising provider
    Change-Id: Ie80d80b4e76c9a84088b2010ed9d910fea4c74e3

---
+++ src/components/AdvertisingProvider.test.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import expectSnapshot from '@mt-testutils/expect-snapshot';
+import AdvertisingProvider from './AdvertisingProvider';
+import { spy } from 'sinon';
+import { mount } from 'enzyme';
+import config from '../utils/testAdvertisingConfig';
+
+const mockActivate = spy();
+const mockSetup = spy();
+const mockTeardown = spy();
+const mockConstructor = spy();
+const mockValueSpy = spy();
+
+jest.mock('../AdvertisingContext', () => ({
+    // eslint-disable-next-line react/prop-types
+    Provider({ value, children }) {
+        mockValueSpy(value);
+        return <div id="advertising-context-provider">{children}</div>;
+    }
+}));
+
+jest.mock(
+    '../Advertising',
+    () =>
+        class {
+            constructor(...args) {
+                mockConstructor(...args);
+            }
+            activate(...args) {
+                mockActivate(...args);
+            }
+            setup(...args) {
+                mockSetup(...args);
+            }
+            teardown(...args) {
+                mockTeardown(...args);
+            }
+        }
+);
+
+describe('The AdvertisingProvider component', () => {
+    it('renders correctly', () =>
+        expectSnapshot(
+            <AdvertisingProvider config={config}>
+                <h1>hello</h1>
+            </AdvertisingProvider>
+        ));
+    describe('when mounted', () => {
+        beforeEach(() => mount(<AdvertisingProvider config={config} />));
+        it('constructs an Advertising module with the provided configuration', () =>
+            void mockConstructor.should.have.been.calledWith(config));
+        it('sets up the Advertising module', () => void mockSetup.should.have.been.called);
+        it('uses an AdvertisingContext.Provider to pass the activate method of the advertising module', () =>
+            expect(mockValueSpy.firstCall.args[0]).toMatchSnapshot());
+        afterEach(resetMocks);
+    });
+    describe('when mounted with active = false', () => {
+        beforeEach(() => mount(<AdvertisingProvider config={config} active={false} />));
+        it('constructs an Advertising module with the provided configuration', () =>
+            void mockConstructor.should.not.have.been.called);
+        it('does not set up an Advertising module', () => void mockSetup.should.not.have.been.called);
+        afterEach(resetMocks);
+    });
+});
+
+function resetMocks() {
+    mockConstructor.resetHistory();
+    mockSetup.resetHistory();
+    mockTeardown.resetHistory();
+    mockActivate.resetHistory();
+    mockValueSpy.resetHistory();
+}
