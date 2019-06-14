aff5656cb127126312e35fb92901e150c359a50c
Jason Li <kahli@ebay.com>
2019-06-14 11:11:56 +0200

    add unit tests for AdvertisingSlot

--- src/components/AdvertisingSlot.test.js
+++ src/components/AdvertisingSlot.test.js
@@ -2,29 +2,53 @@ import React from 'react';
 import { spy, match } from 'sinon';
 import expectSnapshot from '@mt-testutils/expect-snapshot';
 import { mount } from 'enzyme';
+import AdvertisingSlot from './AdvertisingSlot';
 
-const id = 'my-id';
+jest.mock('./utils/connectToAdServer', () => Component => Component);
 
 describe('The advertising slot component', () => {
-    let AdvertisingSlot, mockActivate;
+    const ID = 'my-id';
+    const mockActivate = spy();
     beforeEach(() => {
-        mockActivate = spy();
-        jest.mock('./utils/connectToAdServer', () => Component => props => (
-            <Component {...props} activate={mockActivate} />
-        ));
-        AdvertisingSlot = require('./AdvertisingSlot').default;
+        mockActivate.resetHistory();
     });
-    it('renders correctly', () =>
+
+    it('renders correctly', () => {
         expectSnapshot(
-            <AdvertisingSlot id={id} style={{ color: 'hotpink' }} className="my-class">
+            <AdvertisingSlot activate={mockActivate} id={ID} style={{ color: 'hotpink' }} className="my-class">
                 <h1>hello</h1>
             </AdvertisingSlot>
-        ));
-    describe('when mounted', () => {
-        beforeEach(() => mount(<AdvertisingSlot id={id} />));
-        it('calls the activate function with the ID', () => void mockActivate.should.have.been.calledWith(id));
-        it('calls the activate function with a collapse callback', () =>
-            void mockActivate.should.have.been.calledWith(match.any, match.object));
+        );
     });
+
+    describe('when mounted', () => {
+        let slot;
+        beforeEach(() => {
+            slot = mount(<AdvertisingSlot activate={mockActivate} id={ID} />);
+        });
+
+        it('calls the activate function with the ID', () => {
+            mockActivate.should.have.been.calledWith(ID);
+        });
+
+        it('calls the activate function with a collapse callback', () => {
+            mockActivate.should.have.been.calledWith(match.any, match.object);
+        });
+
+        it('calls the new activate function if the new activate function changes', () => {
+            const newMockActivate = spy();
+            slot.setProps({ activate: newMockActivate });
+
+            // setProps is a async operation
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    // eslint-disable-next-line no-unused-expressions
+                    void newMockActivate.should.has.been.calledOnce;
+                    resolve();
+                }, 0);
+            });
+        });
+    });
+
     afterEach(() => jest.resetModules());
 });
