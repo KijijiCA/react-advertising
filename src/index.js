32fd1612e1bb107841a813c98835e1b5fe226ecc
Matt Colman <matt@mattcolman.com>
2020-01-08 23:36:00 +1100

    Improved Code Packaging and Delivery (PR #21)
    * [breaking change] main will now be transpiled unminified CJS code
    * added source maps to prod minified code
    * Update package.json
    
    Co-authored-by: Matt Colman <matt@mattcolman.com>

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as AdvertisingProvider } from './components/AdvertisingProvider';
+export { default as AdvertisingSlot } from './components/AdvertisingSlot';
+export { default as connectToAdServer } from './components/utils/connectToAdServer';
+export { default as AdvertisingConfigPropType } from './components/utils/AdvertisingConfigPropType';
+export { default as AdvertisingSlotConfigPropType } from './components/utils/AdvertisingSlotConfigPropType';
