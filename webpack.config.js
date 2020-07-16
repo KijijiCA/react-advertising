d04c5f2e2bcfea71c28558370e363b3337d51da1
Patrick Hund <pahund@team.mobile.de>
2020-07-16 12:52:12 +0200

    * Library available from CDN through global variable “reactad” * Beta version bump 13
    Change-Id: I20c12f97a4f3657f8c94312696b88a5862275e3a

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'react-advertising.min.js',
         library: 'reactad',
-        libraryTarget: 'var',
+        libraryTarget: 'window',
     },
     externals: {
         react: {
