b16360598a733dffcfa4b86794706be2a43934d2
Patrick Hund <pahund@team.mobile.de>
2021-02-15 17:02:12 +0100

    * security dependency update * using same prettier config as eCG MoVe * refactored AdvertisingSlot into functional component * AdvertisingSlot uses intersection observer to trigger   ad loading when the slot is in the browser viewport * updated browser list * updated GitHub URL and maintainer email address
    Change-Id: I9810607d04d98c966f083c65a55b569232ad7a6c

--- scripts/setVersionInfo.js
+++ scripts/setVersionInfo.js
@@ -5,12 +5,24 @@ const { join } = require('path');
 const { version } = require('../package.json');
 
 [
-    { path: join(__dirname, '../dist/react-advertising.min.js'), versionInfo: `${version}/UMD` },
-    { path: join(__dirname, '../dist/react-advertising.min.js.map'), versionInfo: `${version}/UMD` },
-    { path: join(__dirname, '../lib/utils/logVersionInfo.js'), versionInfo: `${version}/CommonJS` },
-    { path: join(__dirname, '../es/utils/logVersionInfo.js'), versionInfo: `${version}/ES` },
+  {
+    path: join(__dirname, '../dist/react-advertising.min.js'),
+    versionInfo: `${version}/UMD`,
+  },
+  {
+    path: join(__dirname, '../dist/react-advertising.min.js.map'),
+    versionInfo: `${version}/UMD`,
+  },
+  {
+    path: join(__dirname, '../lib/utils/logVersionInfo.js'),
+    versionInfo: `${version}/CommonJS`,
+  },
+  {
+    path: join(__dirname, '../es/utils/logVersionInfo.js'),
+    versionInfo: `${version}/ES`,
+  },
 ].forEach(({ path, versionInfo }) => {
-    const data = readFileSync(path, 'utf-8');
-    const nextData = data.replace(/REPLACE_ME_VERSION_INFO/gm, versionInfo);
-    writeFileSync(path, nextData, 'utf-8');
+  const data = readFileSync(path, 'utf-8');
+  const nextData = data.replace(/REPLACE_ME_VERSION_INFO/gm, versionInfo);
+  writeFileSync(path, nextData, 'utf-8');
 });
