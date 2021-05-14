3bb63059de96eccfcc7b2c282dc709d700ae3150
Patrick Hund <pahund@team.mobile.de>
2021-05-14 16:29:15 +0200

    …squash me…
    Change-Id: I5c317ab8fa6fb4700c0b05ed63b7dbfc50508327

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
-const { NODE_ENV, BABEL_ENV } = process.env;
+const { NODE_ENV, BABEL_ENV, INSTRUMENTED } = process.env;
 
 const useESModules = NODE_ENV !== 'test' && BABEL_ENV !== 'commonjs';
+const useIstanbul = INSTRUMENTED === 'true';
 
 module.exports = {
   presets: [['@babel/env', { modules: false }], '@babel/react'],
@@ -8,6 +9,7 @@ module.exports = {
     '@babel/proposal-object-rest-spread',
     !useESModules && '@babel/plugin-transform-modules-commonjs',
     ['@babel/transform-runtime', { useESModules }],
+    useIstanbul && 'istanbul',
   ].filter(Boolean),
   env: {
     production: {
